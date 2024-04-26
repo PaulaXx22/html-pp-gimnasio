185158fc86ce5d29cceaf667ce40e0e218b09291
PaulaXx22 <166915419+PaulaXx22@users.noreply.github.com>
2024-04-26 22:14:12 +0200

    Add files via upload

---
+++ fundamentos-de-programacion-bikram-var-bucles-condicionales/index.js
@@ -0,0 +1,71 @@
+let variableSinValor
+let booleano1 = true
+let booleano2 = true
+const PI = 3.14
+const TAU = 2*PI
+let booleanoAnd = true
+booleanoAnd === booleano1 && booleano2
+let booleanoNot = false
+booleanoNot === booleano1
+let booleanoMix0 = true
+booleanoMix0 === (booleano1 || booleano2) && (booleano1 || (!booleano1 && !booleano2))
+let incrementarDesp = 2
+resultadoDesp = incrementarDesp++
+let incrementarAntes = 2
+resultadoAntes = ++incrementarAntes
+
+/*Bucles*/
+let contarHasta10_2 = 0
+for ( let i=0; i < 10; i++ ) {
+    contarHasta10_2++
+}
+
+let postI = 0
+let postJ = 0
+for ( let i=0; i < 11; i++ ) {
+
+    postI=postI+postJ++}
+    
+    let sumaPares=0
+    for ( let i=0; i < 10; i++ ) {
+        if (i % 2=== 0)
+        sumaPares += i
+    }
+    //proyecto individual\\
+    let variableValorNumerico = 1
+    const MiNombre= 'Paula'
+    const MiNumeroFav = 22
+    let booleanoOr= true
+    booleanoOr===booleano1||booleano2
+    booleanoMix1=true
+
+   booleanoMix1 = (booleano1 && (TAU/2 == PI)) || (variableValorNumerico>=MiNumeroFav)
+   let seisNoEsNueve= 6!==9
+   let booleanoMix2 = (variableValorNumerico>0 || variableValorNumerico < -(MiNumeroFav*TAU))
+   let valorSuma = MiNumeroFav + variableValorNumerico
+
+let valorResta = MiNumeroFav - variableValorNumerico
+
+let valorMultiplicacion = MiNumeroFav * variableValorNumerico
+
+let valorDivision = MiNumeroFav / 3
+
+
+
+let contarHasta10 = 0
+while ( contarHasta10 !== 10 ) {
+        ++contarHasta10
+}
+
+let preI = 0
+let preJ = 0
+for ( let i=0; i < 11; i++ ) {
+    preI = preI + ++preJ
+}
+
+
+let sumaImpares = 0
+for (let i=0; i < 10; i++) {
+    if ( i % 2 !== 0)
+    sumaImpares = sumaImpares + i
+}
